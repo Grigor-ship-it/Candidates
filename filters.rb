b39826f45439681fab22cf2953137cc88c7dd18a
Grigor Baltadjian <gb795@hotmail.com>
2021-05-22 20:57:11 +0000

    add functions to filter through hashes

--- filters.rb
+++ filters.rb
@@ -2,15 +2,82 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-  # Your code Here
+  puts id
+  @candidates.each do |candidate|
+    if candidate[:id] == id 
+      return candidate
+    end
+  end
+  puts "No candidate found with that ID"
+  return nil
 end
 
 def experienced?(candidate)
-  # Your code Here
+  
+  if candidate[:years_of_experience] >= 2
+
+    return true
+  else
+
+    return false
+  end
+end
+
+def age(candidate)
+  
+  if candidate[:age] >= 18
+
+    return true
+  else
+
+    return false
+  end
+end
+
+def github_points(candidate)
+  
+  if candidate[:github_points] >= 100
+
+    return true
+  else
+
+    return false
+  end
+end
+
+def languages(candidate)
+  
+  if candidate[:languages].include? "Ruby" || "Python"
+
+    return true
+  else
+
+    return false
+  end
+end
+
+def applied_date(candidate)
+   
+  if Date.today - candidate[:date_applied] >= 15 
+
+    return false
+  else
+
+    return true
+  end
 end
 
 def qualified_candidates(candidates)
-  # Your code Here
+  @candidates.each do |candidate|
+  if experienced?(candidate) && age(candidate) && github_points(candidate) && languages(candidate) && applied_date(candidate) == true
+    puts candidate
+  end
+  end
+end
+
+def ordered_by_qualifications(candidates)
+ puts candidates.sort_by { |h | [h[:years_of_experience], h[:github_points]] }
+  
 end
 
 # More methods will go below
--- main.rb
+++ main.rb
@@ -5,5 +5,24 @@ require './candidates'
 require './filters'
 
 ## Your test code can go here
+# find(4)
+# experienced?( {
+#   id: 5,
+#   years_of_experience: 4,
+#   github_points: 293,
+#   languages: ['C', 'Ruby', 'Python', 'Clojure'],
+#   date_applied: 5.days.ago.to_date,
+#   age: 26
+# })
+# qualified_candidates(@candidates)
+# applied_date( {
+#     id: 5,
+#     years_of_experience: 4,
+#     github_points: 293,
+#     languages: ['C', 'Ruby', 'Python', 'Clojure'],
+#     date_applied: 5.days.ago.to_date,
+#     age: 26
+#   })
+ordered_by_qualifications(@candidates)
 
-pp @candidates
+# pp @candidates
